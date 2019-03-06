06b19a36f9dafbf010ca9d9b64c70b6461f61170
bigpoppa-sys <48267173+bigpoppa-sys@users.noreply.github.com>
2019-03-06 19:10:07 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,7 +11,7 @@ update_system(){
 }
 
 install_ufw(){
-  echo "ISNTALLING UFW"
+  echo "INSTALLING UFW"
   sudo apt-get install ufw -y
   sudo ufw default deny incoming
   sudo ufw default allow outgoing
@@ -33,7 +33,6 @@ install_swap(){
     sudo mount -a
 }
 
-}
 install_dependencies(){
   echo "ISNTALLING DEPENDENCIES"
   # git
@@ -59,14 +58,8 @@ git_clone_repository(){
   fi
 }
 
-syscoin_branch(){
-  read -e -p "Syscoin Core Github Branch [ testnet ]: " SYSCOIN_BRANCH
-  if [ "$SYSCOIN_BRANCH" = "" ]; then
-    SYSCOIN_BRANCH="dev-4.x"
-  fi
-}
-
 git_checkout_branch(){
+  echo "CHECKING OUT BRANCH"
   cd ~/syscoin
   git fetch
   git checkout $SYSCOIN_BRANCH --quiet
@@ -80,21 +73,78 @@ git_checkout_branch(){
 }
 
 autogen(){
+  echo "COMPLETING AUTOGEN"
   cd ~/syscoin
   ./autogen.sh
   clear
 }
 
 configure(){
+  echo "COMPLETING CONFIGURE"
   cd ~/syscoin
   ./configure --without-gui
   clear
 }
 
 compile(){
-  echo "Running compile with $(nproc) core(s)..."
+  echo "COMPLETING MAKE"
   cd ~/syscoin
   make -j$(nproc) -pipe
+  clear
+}
+
+create_syscoin_config(){
+  echo "CREATING SYSCOIN CONF"
+  mkdir ~/.syscoin
+  echo "$SYSCOIN_CONF" > ~/.syscoin/syscoin.conf
+}
+
+start_syscoind(){
+  echo "STARTING SYSCOIND"
+  cd ~/syscoin/src
+  ./syscoind -daemon
+  echo "Waiting 15secs for syscoind to start up"
+  sleep 15s
+  clear
+}
+
+get_masternode_status(){
+  echo "CHECKING GETH AND MNSYNC"
+  cd ~/syscoin/src
+  ./syscoin-cli getblockchaininfo && \
+  ./syscoin-cli mnsync status
+  echo ""
+  read -e -p "Check again? Make sure geth is synced and mnsync is finished, then press n and enter [Y/n]: " CHECK_AGAIN
+  if [ "$CHECK_AGAIN" = "" ] || [ "$CHECK_AGAIN" = "y" ] || [ "$CHECK_AGAIN" = "Y" ]; then
+    get_masternode_status
+  fi
+}
+
+stop_syscoind(){
+  echo "STOPPING SYSCOIND AND ADJUSTING SYSCOIN.CONF & MASTERNODE.CONF"
+  cd ~/syscoin/src
+  ./syscoin-cli stop
+  echo "$SYSCOIN_CONF_MN" > ~/.syscoin/syscoin.conf
+  echo "$MASTERNODE_CONF" > ~/.syscoin/masternode.conf
+}
+
+install_sentinel(){
+  echo "INSTALLING SENTINEL"
+  ~/syscoin/src/syscoind
+  echo "Waiting 15secs for syscoind to start up"
+  sleep 15s
+  cd ~/syscoin/src
+  sudo apt-get update
+  sudo apt-get install -y git python-virtualenv
+  git clone https://github.com/syscoin/sentinel.git
+  cd sentinel
+  git checkout sys4
+  echo "$SENTINEL_CONF" > sentinel.conf
+  echo "installed sentinel conf"
+  sudo apt-get install -y virtualenv
+  virtualenv venv
+  venv/bin/pip install -r requirements.txt
+  venv/bin/python bin/sentinel.py
   clear
 }
 
@@ -109,13 +159,44 @@ gethtestnet=1
 EOF
 )
 
+# syscoin.conf value new defaults
+SYSCOIN_CONF_MN=$(cat <<EOF
+rpcuser=username
+rpcpassword=password
+rpcport=8370
+addnode=54.203.169.179
+addnode=54.190.239.153
+gethtestnet=1
+listen=1
+server=1
+daemon=1
+maxconnections=24
+#
+masternode=1
+masternodeprivkey=5KTY4BjXSkWyikEPKJSBUfrqqT9KXeyobj9yBFndwN1g6usQciH
+externalip=157.230.220.228
+port=8369
+EOF
+)
 
-create_syscoin_config(){
-  echo "CREATING SYSCOIN CONF"
-  mkdir ~/.syscoin
-  echo "$SYSCOIN_CONF" > ~/.syscoin/syscoin.conf
-}
+MASTERNODE_CONF=$(cat <<EOF
+#please delete below as this is just fillers just to get syscoind running properly during startup
+mn1 157.230.220.228:8369 5KTY4BjXSkWyikEPKJSBUfrqqT9KXeyobj9yBFndwN1g6usQciH fff7c491f1a14cf440625dda9d2a450151aa9ab40b623e23763f320a2f007871 1
+EOF
+)
 
+# SENTINEL.conf value defaults
+SENTINEL_CONF=$(cat <<EOF
+# syscoin conf location
+syscoin_conf=/home/root/.syscoin/syscoin.conf
+
+# db connection details
+db_name=/home/root/sentinel/database/sentinel.db
+db_driver=sqlite
+
+# network
+EOF
+)
 
 SYSCOIN_BRANCH="dev-4.x"
 
@@ -136,6 +217,19 @@ autogen
 configure
 compile
 create_syscoin_config
+start_syscoind
 clear
 
+#check sync and move on
+get_masternode_status
+stop_syscoind
+install_sentinel
+
 echo "Just a couple more things to do!."
+echo ""
+echo "1. Create your Masternode Genkey and address - get your tSys"
+echo "2. Edit syscoin.conf for your genkey and your IP address"
+echo "3. Edit masternode.conf"
+echo "4. Restart syscoind"
+echo "5. Recheck mnsync"
+echo "6. Once synced - masternode start-all"
