2b26f0c195f966e14663ee59be77fd28e029b5eb
bigpoppa-sys <48267173+bigpoppa-sys@users.noreply.github.com>
2019-03-06 13:18:08 +1100

    Update mn-script.sh

--- mn-script.sh
+++ mn-script.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-
-update_system(){
-  echo "UPDATING SYSTEM"
-  sudo DEBIAN_FRONTEND=noninteractive apt -y update
-  sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade
-  sudo DEBIAN_FRONTEND=noninteractive apt -y autoremove
-  sudo apt install git -y
-  clear
-}
